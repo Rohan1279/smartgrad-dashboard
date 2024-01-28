85cf00980073e0ed6c5dd285eab83eef155aa3e7
Md Sakib Hossain <40665494+shfsakib@users.noreply.github.com>
2024-01-28 22:59:58 +0600

    Update DashboardLayout.jsx

--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -9,7 +9,7 @@ const DashboardLayout = () => {
       <div className="fixed bottom-0 sm:bottom-auto z-50 pointer-events-none sm:pointer-events-auto sm:absolute flex items-end sm:items-center justify-center w-full sm:w-auto  h-screen">
         <DashboardDrawer />
       </div>
-      <div className="pt-24 min-h-[100svh] px-4 sm:px-12 grid grid-col-5 mmd:grid-cols-7 justify-items-center pb-10">
+      <div className="pt-8 min-h-[100svh] px-4 sm:px-12 grid grid-col-5 mmd:grid-cols-7 justify-items-center pb-10">
         <div className="col-span-1 w-full "></div>
         <div className="col-span-6 xl:col-span-5">
           <Outlet />
