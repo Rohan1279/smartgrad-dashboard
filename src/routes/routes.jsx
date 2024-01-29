1199a5a73931ab5fc2aeb463ab8aaa3ab2696e2f
Rohan1279 <muniruddinrohan@gmail.com>
2024-01-29 13:53:04 +0600

    Update links and styles in DashboardDrawer, Tabs, and routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -56,7 +56,7 @@ const router = createBrowserRouter([
         element: <Profile />,
       },
       {
-        path: "university/:id",
+        path: "university",
         element: <UniversityDashboard />,
       },
       {
