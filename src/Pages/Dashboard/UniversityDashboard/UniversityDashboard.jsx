a700088eb4550ce0d44d3a4f79985b04ed7c6ed4
Rohan1279 <muniruddinrohan@gmail.com>
2024-01-29 17:42:45 +0600

    Add tabs and content to UniversityDashboard component

--- src/Pages/Dashboard/UniversityDashboard/UniversityDashboard.jsx
+++ src/Pages/Dashboard/UniversityDashboard/UniversityDashboard.jsx
@@ -70,7 +70,7 @@ const UniversityDashboard = () => {
   }, []);
 
   return (
-    <div className="text-[#595959]">
+    <div className="text-[#595959] min-h-screen">
       <div className="flex flex-col justify-center md:flex-row md:justify-start items-center space-x-12">
         <img src={DashboardAvatar} alt="avatar" className="w-40" />
 
@@ -84,59 +84,129 @@ const UniversityDashboard = () => {
           </p>
         </div>
       </div>
+      <div className=" min-h-screen  bg-[#F5F5F5] mt-5 px-9 py-5 rounded-xl">
+        <Tabs defaultValue="apply-form" className="">
+          <TabsList>
+            <TabsTrigger
+              className="mr-[42px] relative group"
+              value="apply-form"
+            >
+              Apply
+              <hr className="border mt-[10px] border-[#09D5D7] w-1/2 absolute -bottom-[11px] translate-x-1/2 group-data-[state=active]:block hidden" />
+            </TabsTrigger>
+            <TabsTrigger
+              className="mr-[42px] relative group"
+              value="recommended"
+            >
+              Rocommended by{" "}
+              <span className="text-[#09D5D7] font-bold">smart</span>
+              <span className="font-bold">grad</span>
+              <hr className="border mt-[10px] border-[#09D5D7] w-1/2 absolute -bottom-[11px] translate-x-1/2  group-data-[state=active]:block hidden" />
+            </TabsTrigger>
+            <TabsTrigger
+              className="mr-[42px] relative group"
+              value="application"
+            >
+              Applications
+              <hr className="border mt-[10px] border-[#09D5D7] w-1/2 absolute -bottom-[11px] translate-x-1/2  group-data-[state=active]:block hidden" />
+            </TabsTrigger>
+          </TabsList>
+          <hr className="w-full  border mt-[10px] border-[#D9D9D9]" />
 
-      <Tabs defaultValue="apply-form" className="mt-5 pl-[10px]">
-        <TabsList>
-          <TabsTrigger className="mr-[42px] relative group" value="apply-form">
-            Apply
-            <hr className="border mt-[10px] border-[#09D5D7] w-1/2 absolute -bottom-[11px] translate-x-1/2 group-data-[state=active]:block hidden" />
-          </TabsTrigger>
-          <TabsTrigger className="mr-[42px] relative group" value="recommended">
-            Rocommended by{" "}
-            <span className="text-[#09D5D7] font-bold">smart</span>
-            <span className="font-bold">grad</span>
-            <hr className="border mt-[10px] border-[#09D5D7] w-1/2 absolute -bottom-[11px] translate-x-1/2  group-data-[state=active]:block hidden" />
-          </TabsTrigger>
-          <TabsTrigger className="mr-[42px] relative group" value="application">
-            Applications
-            <hr className="border mt-[10px] border-[#09D5D7] w-1/2 absolute -bottom-[11px] translate-x-1/2  group-data-[state=active]:block hidden" />
-          </TabsTrigger>
-        </TabsList>
-        <hr className="w-full  border mt-[10px] border-[#D9D9D9]" />
-        <TabsContent value="apply-form">
-          <Form
-            currentForm={currentForm}
-            setCurrentForm={setCurrentForm}
-            formManager={formManager}
-            currentTab={currentTab}
-            id={currentTab}
-            setFormManager={setFormManager}
-          />
-        </TabsContent>
-        <TabsContent value="recommended">
-          <div className="flex justify-start space-x-9 items-center mt-5">
-            {recommendationData.map((item) => {
-              return (
-                <RecommendationCard
-                  key={item.name}
-                  name={item.name}
-                  subject={item.subject}
-                  academic_expense={item.academic_expense}
-                  card_image={item.card_image}
-                  cost={item.cost}
-                  job_permit={item.job_permit}
-                  logo={item.logo}
-                  offer_rate={item.offer_rate}
-                  ratings={item.ratings}
+          <TabsContent value="apply-form">
+            <Form
+              currentForm={currentForm}
+              setCurrentForm={setCurrentForm}
+              formManager={formManager}
+              currentTab={currentTab}
+              id={currentTab}
+              setFormManager={setFormManager}
+            />
+          </TabsContent>
+          <TabsContent value="recommended">
+            <div className="flex justify-start space-x-9 items-center mt-10 ">
+              {recommendationData.map((item) => {
+                return (
+                  <RecommendationCard
+                    key={item.name}
+                    name={item.name}
+                    subject={item.subject}
+                    academic_expense={item.academic_expense}
+                    card_image={item.card_image}
+                    cost={item.cost}
+                    job_permit={item.job_permit}
+                    logo={item.logo}
+                    offer_rate={item.offer_rate}
+                    ratings={item.ratings}
+                  />
+                );
+              })}
+            </div>
+          </TabsContent>
+          <TabsContent value="application">
+            <div className="flex items-center justify-between bg-white p-4 rounded-xl mt-10 ">
+              <div className="flex items-center space-x-4">
+                <img
+                  alt="University Logo"
+                  className="h-12 w-12 rounded-full"
+                  height="50"
+                  src="/placeholder.svg"
+                  style={{
+                    aspectRatio: "50/50",
+                    objectFit: "cover",
+                  }}
+                  width="50"
                 />
-              );
-            })}
-          </div>
-        </TabsContent>
-        <TabsContent value="application">Applications</TabsContent>
-      </Tabs>
+                <div>
+                  <h2 className="text-lg font-semibold">
+                    Master of Business Administration
+                  </h2>
+                  <p className="text-sm text-gray-600">
+                    Southeast Minnesota State University
+                  </p>
+                  <div className="flex items-center mt-1">
+                    <StarIcon className="text-yellow-400 h-5 w-5" />
+                    <StarIcon className="text-yellow-400 h-5 w-5" />
+                    <StarIcon className="text-yellow-400 h-5 w-5" />
+                    <StarIcon className="text-yellow-400 h-5 w-5" />
+                    <StarIcon className="text-gray-300 h-5 w-5" />
+                  </div>
+                  <p className="text-sm text-gray-600 mt-1">
+                    Applied on June 23, 2023
+                  </p>
+                </div>
+              </div>
+              <div className="text-right">
+                <p className="text-sm font-medium">Application ID : 30459</p>
+                <p className="text-sm text-blue-600">
+                  status : Application In-review
+                </p>
+              </div>
+            </div>
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 };
 
 export default UniversityDashboard;
+
+function StarIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+    </svg>
+  );
+}
