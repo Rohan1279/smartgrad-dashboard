3cd0af8cfc574901910a22969b305714a2b508c5
Rohan1279 <muniruddinrohan@gmail.com>
2024-01-30 22:43:59 +0600

    Add DocumentsUploadField component to Documents.jsx

--- src/Pages/Dashboard/UniversityDashboard/ApplicationTab/TabContents/Documents.jsx
+++ src/Pages/Dashboard/UniversityDashboard/ApplicationTab/TabContents/Documents.jsx
@@ -1,4 +1,5 @@
 import DocumentStatusField from "@/components/Dashboard/DocumentStatusField/DocumentStatusField";
+import DocumentsUploadField from "@/components/Dashboard/DocumentsUploadField/DocumentsUploadField";
 
 const Documents = () => {
   return (
@@ -28,7 +29,30 @@ const Documents = () => {
           <DocumentStatusField name="Recommendation Letter" status={true} />
         </div>
       </div>
-      <h2 className="text-3xl font-bold">Required Documents</h2>
+      <div className="mb-14">
+        <h2 className="text-3xl font-bold">Required Documents</h2>
+        <DocumentsUploadField
+          label={"Upload SOP"}
+          name={"sop"}
+          tooltip={"Let Smartgrad help you writing an SOP Letter"}
+          link={"https://smartgrad.in"}
+        />
+        <DocumentsUploadField
+          label={"Upload Your CV"}
+          name={"cv"}
+          tooltip={"Let Smartgrad help you writing an CV"}
+          link={"https://smartgrad.in"}
+        />
+        <DocumentsUploadField
+          label={"Upload your MOI"}
+          name={"moi"}
+          tooltip={"Let Smartgrad help you writing an MOI Letter"}
+          link={"https://smartgrad.in"}
+        />
+      </div>
+      <button className="w-[98.46px] h-[38.61px] bg-zinc-600 rounded-[5px] text-white">
+        Next
+      </button>
     </div>
   );
 };
