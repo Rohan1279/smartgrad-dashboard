abec3cdf814bbd2775de470b1864ca8145c33abe
Rohan1279 <muniruddinrohan@gmail.com>
2024-02-12 00:36:04 +0600

    add changes

--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -14,7 +14,7 @@ const DashboardLayout = () => {
       className="relative "
     >
       <Navbar />
-      <div className="fixed bottom-0 mmd:bottom-auto z-50 pointer-events-none mmd:pointer-events-auto mmd:absolute flex items-end mmd:items-center justify-center w-full mmd:w-auto  h-screen">
+      <div className="absolute bottom-0 mmd:bottom-auto z-50 pointer-events-none mmd:pointer-events-auto mmd:fixed flex items-end mmd:items-center justify-center w-full mmd:w-auto  h-screen">
         <DashboardDrawer />
       </div>
       <div className="pt-4 sm:pt-8 min-h-[100svh] px-4 sm:px-12 grid grid-col-5 mmd:grid-cols-7 justify-items-center pb-16 mmd:pb-10">
