3005a7e94deaa269f6b393dbd710483f91936894
JOY-JB <joybarua0058@gmail.com>
2024-02-16 22:37:35 +0600

    notification redesign

--- src/Shared/Navbar/Menus/NotificationMenu.jsx
+++ src/Shared/Navbar/Menus/NotificationMenu.jsx
@@ -1,18 +1,35 @@
-import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
 import NotificationIcon from "/assets/images/navbar/notification/notification.svg";
-import NotificationIconActive from "/assets/images/navbar/notification/notification-active.svg";
-import UniversityNotifyIcon from "/assets/images/navbar/notification/university-warning.png";
-import AptitudesNotifyIcon from "/assets/images/navbar/notification/aptitudes-alert.png";
-import UniversitiesIcon from "/assets/images/navbar/convocation-cap.svg";
 
+import axios from "@/api/axios";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import notificationIcons from "@/utils/Icon";
 
 const NotificationMenu = () => {
+  const [notifications, setNotifications] = useState([]);
+  
+
+
+  useEffect(() => {
+    axios
+      .get("/notifications/", {
+        params: {
+          token: localStorage.getItem("token"),
+        },
+      })
+      .then(({ data }) => {
+        setNotifications(data);
+      });
+  }, []);
+
+  const name = "John Doe";
+  name.substring(0, 1);
+
   return (
     <div>
       <DropdownMenu>
@@ -23,19 +40,16 @@ const NotificationMenu = () => {
             className="w-5 cursor-pointer transition-all outline-none"
           />
         </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem className="flex space-x-3 cursor-pointer">
-            <img src={UniversityNotifyIcon} alt="" />
-            <span>You got 3 pending requests </span>
-          </DropdownMenuItem>
-          <DropdownMenuItem className="flex space-x-3 cursor-pointer">
-            <img src={AptitudesNotifyIcon} alt="" />
-            <span>You have 2 due submissions</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem className="flex space-x-3 cursor-pointer">
-            <img src={UniversitiesIcon} alt="" />
-            <span>Check out recommendations</span>
-          </DropdownMenuItem>
+        <DropdownMenuContent className="max-w-96">
+          
+          {
+            notifications.map((notification, key) => (
+              <DropdownMenuItem key={key} className="flex space-x-3 cursor-pointer">
+                {<img className="w-8" src={notificationIcons[notification.type][notification.level]} alt="" />}
+                <span>{notification?.text}</span>
+              </DropdownMenuItem>
+            ))
+          }
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
