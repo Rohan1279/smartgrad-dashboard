c2777f20ba7933f0d2fc7d10e9afa55cbb2f650e
JOY-JB <joybarua0058@gmail.com>
2024-02-21 21:31:42 +0600

    dashboard sidebar content adjusment

--- src/components/Dashboard/TabContainer/DashCardContent.jsx
+++ src/components/Dashboard/TabContainer/DashCardContent.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import NetworksCard from "../ThemeCards/NetworksCard/NetworksCard";
+import NewEventsCard from "../ThemeCards/NewEvents/NewEvents";
 import UniversitiesCard from "../ThemeCards/UniversitiesCard/UniversitiesCard";
 
 const DashCardContent = () => {
   return (
     <Fragment>
       <div className="col-span-3 lg:col-span-1 mmd:ml-[25px] bg- rounded-lg lg:bg-inherit h-fit">
-        <div className="grid gap-x-2 gap-y-6 grid-rows-1 px-4 mmd:px-0 pt-4 md:pt-0 pb-4 ">
+        <div className="flex flex-col gap-5 px-4 mmd:px-0 pt-4 md:pt-0 h-[85vh]"> 
+          <NewEventsCard />
           <UniversitiesCard />
           {/* <CoursesCard /> */}
           {/* <OpputunitiesCard /> */}
