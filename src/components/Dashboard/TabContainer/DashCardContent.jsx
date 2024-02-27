279ed7deb2d4d1a76223d90f917f7e1794e347f7
JOY-JB <joybarua0058@gmail.com>
2024-02-28 02:37:52 +0600

    newEvent card size change

--- src/components/Dashboard/TabContainer/DashCardContent.jsx
+++ src/components/Dashboard/TabContainer/DashCardContent.jsx
@@ -7,7 +7,7 @@ const DashCardContent = () => {
   return (
     <Fragment>
       <div className="col-span-3 lg:col-span-2 xl:col-span-1 mmd:ml-[25px] bg- rounded-lg lg:bg-inherit h-fit">
-        <div className="flex flex-col gap-2 xl:gap-3 px-4 mmd:px-0 pt-4 md:pt-0 h-[85vh]"> 
+        <div className="flex flex-col gap-2 xl:gap-3 px-4 mmd:px-0 pt-4 md:pt-0 "> 
           <NewEventsCard />
           <UniversitiesCard />
           {/* <CoursesCard /> */}
