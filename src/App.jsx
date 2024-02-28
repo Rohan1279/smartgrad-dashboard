e29f6c99c87b64cf389ea6ffcf8ea4cbe74bde2d
Rohan1279 <muniruddinrohan@gmail.com>
2024-02-29 01:50:39 +0600

    add react-chatbot-kit

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,13 @@ import "react-photo-view/dist/react-photo-view.css";
 import Modal from "./components/Modal/Modal";
 import ContextProvider from "./contexts/ContextProvider";
 import { useEffect } from "react";
+// import config from "../components/Chatbot/config";
+// import MessageParser from "../components/Chatbot/MessageParser";
+// import ActionProvider from "../components/Chatbot/ActionProvider";
+import Chatbot from "react-chatbot-kit";
+import config from "./components/Chatbot/config";
+import MessageParser from "./components/Chatbot/MessageParser";
+import ActionProvider from "./components/Chatbot/ActionProvider";
 
 function App() {
   return (
@@ -15,6 +22,13 @@ function App() {
         <RouterProvider router={router} />
         <Toaster richColors />
         <Modal />
+        <div className="fixed bottom-5 right-5  ">
+          <Chatbot
+            config={config}
+            messageParser={MessageParser}
+            actionProvider={ActionProvider}
+          />
+        </div>
       </ContextProvider>
     </>
   );
