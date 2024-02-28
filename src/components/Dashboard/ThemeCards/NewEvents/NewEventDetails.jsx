c10cbfd3de6f9a5cc5e64edc92e0b79b13a628ed
JOY-JB <joybarua0058@gmail.com>
2024-02-29 03:42:21 +0600

    new event

--- src/components/Dashboard/ThemeCards/NewEvents/NewEventDetails.jsx
+++ src/components/Dashboard/ThemeCards/NewEvents/NewEventDetails.jsx
@@ -6,11 +6,13 @@ const NewEventDetails = ({ item }) => {
     <Fragment>
       <Link to={item.url} className="">
         <img src={item.image} alt="event" className="w-full h-44 rounded-t-lg" />
-
+        
+        <div className="p-2">
         <p className="mt-3 text-md font-semibold line-clamp-1">Resources and Strategies for Higher Score.
         </p>
         <p className="text-slate-400 line-clamp-1 ">Deadline: 12th August, 2021
         </p>
+        </div>
 
       </Link>
     </Fragment>
