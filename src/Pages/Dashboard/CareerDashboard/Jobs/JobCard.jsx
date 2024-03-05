794a69e9d58d2f37c37e477b570e26686da95515
JOY-JB <joybarua0058@gmail.com>
2024-03-05 23:38:20 +0600

    change description structure and remove react select related dependency

--- src/Pages/Dashboard/CareerDashboard/Jobs/JobCard.jsx
+++ src/Pages/Dashboard/CareerDashboard/Jobs/JobCard.jsx
@@ -5,11 +5,11 @@ import { IoLocationOutline } from "react-icons/io5";
 const JobCard = ({ data }) => {
   const { image, title, company, location, type } = data;
   return (
-    <div className="flex items-center border-b space-x-2 py-2 cursor-pointer">
+    <div className="flex items-center border-b space-x-2 py-4 cursor-pointer">
       <img src={image} alt="" className="w-12 h-12 rounded-full" />
       <div className="">
         <h1 className="text-primary font-semibold">{title}</h1>
-        <div className="flex items-center">
+        <div className="flex items-center my-0.5">
           <FaBriefcase size={14} className="mb-1 mr-2" />
           {company}
         </div>
--- src/Pages/Dashboard/CareerDashboard/Jobs/JobDetails.jsx
+++ src/Pages/Dashboard/CareerDashboard/Jobs/JobDetails.jsx
@@ -1,3 +1,4 @@
+import parse from 'html-react-parser';
 import PropTypes from "prop-types";
 import { FaBriefcase } from "react-icons/fa6";
 import { IoLocationOutline } from "react-icons/io5";
@@ -10,7 +11,7 @@ const JobDetails = ({data}) => {
                 <img src={data.image} alt="" className="w-14 h-14 rounded-full" />
                 <div className="">
                     <h1 className="text-primary font-semibold">{data.title}</h1>
-                    <div className="flex items-center">
+                    <div className="flex items-center my-0.5">
                         <FaBriefcase size={14} className="mb-1 mr-2" />
                         {data.company}
                     </div>
@@ -21,36 +22,8 @@ const JobDetails = ({data}) => {
                 </div>
             </div>
             <div className="py-2">
-                <h1 className="text-primary font-semibold">Job Description</h1>
-                <p className="text-sm">{data?.details?.description}</p>
-            </div>
-            <div className="py-2">
-                <h1 className="text-primary font-semibold">Key Responsibilities</h1>
-                <ul className="list-inside list-disc">
-                    {data.details["keyResponsibilities"].map((item, index) => (
-                        <li key={index}>{item}</li>
-                    ))}
-                </ul>
-            </div>
-            <div className="py-2">
-                <h1 className="text-primary font-semibold">Educational Requirements</h1>
-                <ul className="list-inside list-disc">
-                    {data.details["educationalRequirements"].map((item, index) => (
-                        <li key={index}>{item}</li>
-                    ))}
-                </ul>
-            </div>
-            <div className="py-2">
-                <h1 className="text-primary font-semibold">Required Experience</h1>
-                <ul className="list-inside list-disc">
-                    {data.details["requiredExperience"].map((item, index) => (
-                        <li key={index}>{item}</li>
-                    ))}
-                </ul>
-            </div>
-            <div className="py-2">
-                <h1 className="text-primary font-semibold">Company Details</h1>
-                <p className="text-sm">{data.details.employees}</p>
+                {/* <div id="htmDetails"></div> */}
+                {parse(data?.details)}
             </div>
         </div>
     );
@@ -65,12 +38,6 @@ JobDetails.propTypes = {
       company: PropTypes.string.isRequired,
       location: PropTypes.string.isRequired,
       type: PropTypes.string.isRequired,
-      details: PropTypes.shape({
-        employees: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        keyResponsibilities: PropTypes.arrayOf(PropTypes.string).isRequired,
-        educationalRequirements: PropTypes.arrayOf(PropTypes.string).isRequired,
-        requiredExperience: PropTypes.arrayOf(PropTypes.string).isRequired,
-      }).isRequired,
+      details: PropTypes.string.isRequired,
     }).isRequired,
   };
